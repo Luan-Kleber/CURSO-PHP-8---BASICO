94d02dc12ffc16c8c2ee0ca920310cab8f0ec77c
Luan <luanamaral.6540@hotmail.com>
2024-01-31 07:21:01 -0300

    aula12

---
+++ include_require/inclusao_cuidado/inclusao.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Ainda acerca de inclusão de scripts na nossa aplicação, é muito importante ter em atenção
+os caminhos (paths) dos nosso scripts.
+
+IMPORTANTE: Existem várias formas de definir corretamente os caminhos.
+Veremos essas formas mais à frente e outros módulos de PHP.
+
+Vejamos os exemplos.
+*/
+
+?>
---
+++ include_require/inclusao_uso/exemplo2/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <nav>
+        <a href="index.php">Inicio</a>
+        <a href="about.php">Acerca de</a>
+        <a href="contact.php">Contatos</a>
+    </nav>
+
+    <!-- 
+        A utilidade do exemplo 2 é que se eu precisa alterar algo do cabeçalho vai mudar para todas as paginas.
+        Diferente do exemplo 1 onde eu precisaria alterar 1 de cada vez.
+    -->
