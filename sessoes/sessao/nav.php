1a18968e995567499542ce0cb82d3c253da707f2
Luan <luanamaral.6540@hotmail.com>
2024-01-31 21:19:25 -0300

    aula12

---
+++ sessoes/sessao/nav.php
@@ -0,0 +1,7 @@
+<nav style="display: flex; flex-direction: column;">
+    <a href="index.php">Inicio</a>
+    <a href="adicionar1.php">Colocar valor 1 na sessão</a>
+    <a href="adicionar2.php">Colocar valor 2 na sessão</a>
+    <a href="remover1.php">Remover valor 2 na sessão</a>
+    <a href="destruir.php">Destruir toda a sessão</a>
+</nav>
